dc907b43c932180f43fa3ef0510da30b655e8049
Erlend Åmdal <erlend@aamdal.com>
2016-08-21 23:38:49 +0200

    Add oak stepladder as creative tab icon

--- src/main/kotlin/com/jamieswhiteshirt/literalascension/common/CreativeTab.kt
+++ src/main/kotlin/com/jamieswhiteshirt/literalascension/common/CreativeTab.kt
@@ -1,9 +1,9 @@
 package com.jamieswhiteshirt.literalascension.common
 
+import com.jamieswhiteshirt.literalascension.common.init.LAItems
 import net.minecraft.creativetab.CreativeTabs
-import net.minecraft.init.Blocks
 import net.minecraft.item.Item
 
 object CreativeTab : CreativeTabs("literalascension.tab") {
-    override fun getTabIconItem(): Item = Item.getItemFromBlock(Blocks.LADDER)!!
+    override fun getTabIconItem(): Item = LAItems.OAK_STEPLADDER
 }
