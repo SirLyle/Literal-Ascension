73a00ba9a993b1960f77e2a92c86c9bdaecb9e6f
Erlend Åmdal <erlend@aamdal.com>
2016-11-04 12:51:24 +0100

    Fix carving tool damage multipliers

--- src/main/kotlin/com/jamieswhiteshirt/literalascension/common/features/carving/carvingmaterials/CarvingMaterial.kt
+++ src/main/kotlin/com/jamieswhiteshirt/literalascension/common/features/carving/carvingmaterials/CarvingMaterial.kt
@@ -1,6 +1,5 @@
 package com.jamieswhiteshirt.literalascension.common.features.carving.carvingmaterials
 
-import com.jamieswhiteshirt.literalascension.api.ICarvingBehaviour
 import com.jamieswhiteshirt.literalascension.api.ICarvingMaterial
 import com.jamieswhiteshirt.literalascension.common.SubFeatureCollection
 import com.jamieswhiteshirt.literalascension.common.features.carving.CarvingMaterials
@@ -11,8 +10,8 @@ import net.minecraftforge.common.config.Configuration
 abstract class CarvingMaterial(val block: Block, toolLevel: Int, name: String, config: Configuration, override val parent: CarvingMaterials) : SubFeatureCollection<CarvedBlock>(name, parent), ICarvingMaterial {
     override val requiredCarvingToolLevel: Int = config.getInt("requiredCarvingToolLevel", featureName, toolLevel, 0, 5, "Required tool level for carving of this material")
     override val suitableCarvingToolLevel: Int = config.getInt("suitableCarvingToolLevel", featureName, toolLevel, 0, 5, "Suitable tool level for carving of this material")
-    override val suitableToolDamageMultiplier: Int = config.getInt("suitableCarvingToolDamageMultiplier", featureName, 0, 0, 20, "Tool damage multiplier for suitable carving of this material")
-    override val unsuitableToolDamageMultiplier: Int = config.getInt("suitableCarvingToolDamageMultiplier", featureName, 10, 0, 20, "Tool damage multiplier for unsuitable carving of this material")
+    override val suitableToolDamageMultiplier: Int = config.getInt("suitableCarvingToolDamageMultiplier", featureName, 1, 0, 20, "Tool damage multiplier for suitable carving of this material")
+    override val unsuitableToolDamageMultiplier: Int = config.getInt("unsuitableCarvingToolDamageMultiplier", featureName, 10, 0, 20, "Tool damage multiplier for unsuitable carving of this material")
 
     abstract fun registerCarvingBehaviourShims()
 }
