e35fe28f86d1b3f8125405ec45853b094287a35f
Erlend Åmdal <erlend@aamdal.com>
2016-11-06 18:28:48 +0100

    Add BoP support and fix EnvTech and Chisel chute textures

--- src/main/kotlin/com/jamieswhiteshirt/literalascension/common/features/carving/CarvingDomains.kt
+++ src/main/kotlin/com/jamieswhiteshirt/literalascension/common/features/carving/CarvingDomains.kt
@@ -2,16 +2,14 @@ package com.jamieswhiteshirt.literalascension.common.features.carving
 
 import com.jamieswhiteshirt.literalascension.common.SubFeatureCollection
 import com.jamieswhiteshirt.literalascension.common.features.Carving
-import com.jamieswhiteshirt.literalascension.common.features.carving.carvingdomains.CarvingDomain
-import com.jamieswhiteshirt.literalascension.common.features.carving.carvingdomains.CarvingDomainChisel
-import com.jamieswhiteshirt.literalascension.common.features.carving.carvingdomains.CarvingDomainEnvironmentalTech
-import com.jamieswhiteshirt.literalascension.common.features.carving.carvingdomains.CarvingDomainMinecraft
+import com.jamieswhiteshirt.literalascension.common.features.carving.carvingdomains.*
 import net.minecraftforge.common.config.Configuration
 
 class CarvingDomains(config: Configuration, override val parent: Carving) : SubFeatureCollection<CarvingDomain>("domains", parent) {
     val MINECRAFT = optionalOn(config, CarvingDomainMinecraft(config, this))
     val CHISEL = optionalOn(config, CarvingDomainChisel(config, this))
     val ENVIRONMENTALTECH = optionalOn(config, CarvingDomainEnvironmentalTech(config, this))
+    val BIOMESOPLENTY = optionalOn(config, CarvingDomainBiomesOPlenty(config, this))
 
     init {
         for (carvingDomain in subFeatures) {
---
+++ src/main/kotlin/com/jamieswhiteshirt/literalascension/common/features/carving/carvingdomains/carvableblocks/CarvableBlockBiomesOPlentyLog.kt
@@ -0,0 +1,12 @@
+package com.jamieswhiteshirt.literalascension.common.features.carving.carvingdomains.carvableblocks
+
+import com.jamieswhiteshirt.literalascension.common.features.carving.carvingdomains.CarvingDomainBiomesOPlenty
+import com.jamieswhiteshirt.literalascension.common.features.carving.carvingdomains.carvableblocks.carvableblocktypes.CarvableBlockTypeSimple
+import net.minecraft.block.Block
+import net.minecraftforge.common.config.Configuration
+
+class CarvableBlockBiomesOPlentyLog(block: Block, name: String, types: Array<String>, config: Configuration, parent: CarvingDomainBiomesOPlenty) : CarvableBlockMeta(block, 0, name, config, parent) {
+    override val fromMeta = types.indices.associate {
+        it + 4 to optionalOn(config, CarvableBlockTypeSimple(block.getStateFromMeta(it + 4), types[it], this))
+    }
+}
